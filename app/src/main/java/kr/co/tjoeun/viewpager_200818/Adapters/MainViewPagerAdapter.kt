3eafde5e867a9895b2e235c630b3b5f92e211952
yugyeong ko <ckck0425ko@gmail.com>
2020-08-18 20:56:53 +0900

    뷰페이저에서 프래그먼트들을 뿌려주는 작업

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_200818/Adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package kr.co.tjoeun.viewpager_200818.Adapters
+
+import android.icu.text.Transliterator
+import android.telephony.PhoneNumberFormattingTextWatcher
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.tjoeun.viewpager_200818.Fragments.AddFragment
+import kr.co.tjoeun.viewpager_200818.Fragments.NameFragment
+import kr.co.tjoeun.viewpager_200818.Fragments.PhoneNumFragment
+
+class MainViewPagerAdapter(fm:FragmentManager):FragmentPagerAdapter(fm){
+
+    override fun getItem(position: Int): Fragment{
+
+        if (position==0) {
+            return NameFragment()
+        }
+
+        else if (position==1){
+            return PhoneNumFragment()
+        }
+        else {
+            return AddFragment()
+        }
+
+    }
+
+    override fun getCount():Int{
+        return 3
+    }
+
+
+}
---
+++ app/src/main/java/kr/co/tjoeun/viewpager_200818/Fragments/AddFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.viewpager_200818.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.viewpager_200818.R
+
+class AddFragment  : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+}
