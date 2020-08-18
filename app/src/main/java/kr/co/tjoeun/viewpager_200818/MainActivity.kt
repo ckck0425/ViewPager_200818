bd680a0f7555bb715f541fa840db18cb20aa4a77
yugyeong ko <ckck0425ko@gmail.com>
2020-08-18 21:01:21 +0900

    어댑터 / 뷰페이저 연결 => 실제 페이징처리

--- app/src/main/java/kr/co/tjoeun/viewpager_200818/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/viewpager_200818/MainActivity.kt
@@ -2,15 +2,20 @@ package kr.co.tjoeun.viewpager_200818
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.viewpager_200818.Adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mMainPagerAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupEvents()
-        setValues()
     }
-    override fun setupEvents() {
+
+    override fun setupValues() {
+        mMainPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mMainPagerAdapter
     }
 }
